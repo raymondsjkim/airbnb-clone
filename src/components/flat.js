1fd0703b49f7f6a8d104fdbf2b38a5bfd34a5945
raymondsjkim <raymond.sj.kim@gmail.com>
2018-05-24 01:57:16 -0700

    created flat component

---
+++ src/components/flat.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./flat.css";
+
+class Flat extends React.Component {
+    render() {
+        this.props.flat.name
+        console.log(this.props.flat.name);
+
+        const title = this.props.flat.price 
+            + this.props.flat.priceCurrency 
+            + " - " + this.props.flat.name;
+        // backgroundImage replace css background-image
+        // ` back tick
+        const style = {
+            backgroundImage: `url('${this.props.flat.imageUrl}')`
+        };
+
+
+        return (
+            // JSX className is the same as HTML class
+            <div className="flat"> 
+                <div className="flat-picture" style={style}></div>
+                <div className="flat-title">
+                    {title}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Flat;
--- src/components/hello.js
+++ src/components/hello.js
@@ -3,7 +3,7 @@ import React from "react";
 class Hello extends React.Component {
     render() {
         // TODO
-        return <div>hello from hello.js</div>
+        return <div>hello from {this.props.firstName} {this.props.lastName}</div> // this.props.name value comes from name attribute
     }
 }
 
