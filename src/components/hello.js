02259b9fbc584a7dc3d231105a198eaa489fb00f
raymondsjkim <raymond.sj.kim@gmail.com>
2018-05-24 00:16:02 -0700

    First commit

---
+++ src/components/hello.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Hello extends React.Component {
+    render() {
+        // TODO
+        return <div>hello from hello.js</div>
+    }
+}
+
+export default Hello;
